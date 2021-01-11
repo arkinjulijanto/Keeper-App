212631b0525927797ce8191e6b09ab994ade85f2
Arkin <arkinjulianto@gmail.com>
2021-01-11 20:04:18 +0700

    Updates

---
+++ precache-manifest.05e94c4ec1c1efa2cf46297b03dc3731.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3549d5be5bd741bc6abc2a5cfe9b2c61",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "144d5977913765ecf725",
+    "url": "/Keeper-App/static/js/2.772c138d.chunk.js"
+  },
+  {
+    "revision": "53dde0a83f290f05320d",
+    "url": "/Keeper-App/static/js/main.2e5110e4.chunk.js"
+  },
+  {
+    "revision": "128fd620914684fbc76b",
+    "url": "/Keeper-App/static/js/runtime-main.113ea231.js"
+  }
+]);
